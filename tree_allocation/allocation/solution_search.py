c13a7ed4cbb2b320a146bd7d1e7d5c931d54d234
schlixmann <felixschumann95@gmx.de>
2024-01-18 13:45:29 +0100

    new class solution search

---
+++ tree_allocation/allocation/solution_search.py
@@ -0,0 +1,20 @@
+# Import modules
+
+# Import external packages
+from lxml import etree
+
+class SolutionSearch():
+    pass
+
+class Genetic():
+    pass
+
+class Heuristic():
+    pass
+
+class Brute():
+    pass
+
+
+def solution_search_factory():
+    pass
